d51780d3e384dd211a82f80812caaef2da5b8887
Ryan Sakurai <ryansakurai@estudante.ufscar.br>
2024-03-01 19:09:34 -0300

    implement perc command

--- src/mafs/main.py
+++ src/mafs/main.py
@@ -1,10 +1,41 @@
+"""
+"""
+
 import typer
+from typing_extensions import Annotated
 
 app = typer.Typer()
 
 @app.command()
-def perc():
-    pass
+def perc(
+    whole: Annotated[
+        float,
+        typer.Option("--whole", "-w", show_default=False)
+    ] = None,
+    part: Annotated[
+        float,
+        typer.Option("--part", "-p", show_default=False)
+    ] = None,
+    ratio: Annotated[
+        float,
+        typer.Option("--ratio", "-r", show_default=False)
+    ] = None,
+):
+    """
+    Pass two of the three options and get the third calculated for you.
+    """
+
+    if whole is not None and part is not None:
+        ratio = part/whole
+        print(f"Ratio: {ratio}")
+    elif whole is not None and ratio is not None:
+        part = whole*ratio
+        print(f"Part: {part}")
+    elif part is not None and ratio is not None:
+        whole = part/ratio
+        print(f"Whole: {whole}")
+    else:
+        raise typer.BadParameter("Two options must be specified (eg. whole and part).")
 
 @app.command()
 def rand():
